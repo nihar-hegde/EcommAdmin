9c1deeec62e55745c3d75fbf19d889b3ed786c3c
Nihar Hegde <niharhegde163@gmail.com>
2023-09-12 18:47:58 +0530

    added prisma schema for billboard

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingForm.tsx
@@ -6,102 +6,126 @@ import Heading from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
-import * as z from 'zod'
+import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { toast } from "react-hot-toast";
 import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { AlertModal } from "@/components/modals/alert-modal";
 import ApiAlert from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/use-origin";
 interface SettingFormProps {
   initialData: Store;
 }
 
 const formSchema = z.object({
-    name:z.string().min(1),
-})
+  name: z.string().min(1),
+});
 type SettingFormValues = z.infer<typeof formSchema>;
-
 
 const SettingForm: FC<SettingFormProps> = ({ initialData }) => {
   const params = useParams();
   const router = useRouter();
-    const [open ,setOpen] = useState(false);
-    const [loading ,setLoading] = useState(false);
-    const form = useForm<SettingFormValues>({
-        resolver:zodResolver(formSchema),
-        defaultValues:initialData
-    });
-    const onSubmit = async (data:SettingFormValues)=>{
-        try {
-          setLoading(true);
-          await axios.patch(`/api/stores/${params.storeId}`,data);
-          router.refresh();
-          toast.success("Store Updated")
-        } catch (error) {
-          toast.error('Something went wrong');
-        }finally{
-          setLoading(false);
-        }
+  const origin = useOrigin();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const form = useForm<SettingFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+  const onSubmit = async (data: SettingFormValues) => {
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+      toast.success("Store Updated");
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
     }
-    const onDelete = async()=>{
-      try {
-        setLoading(true);
-        await axios.delete(`/api/stores/${params.storeId}`)
-        router.refresh()
-        router.push('/');
-        toast.success("Store Deleted!!!")
-      } catch (error) {
-        toast.error("Make sure you have removed all products and categories first.")
-      }finally{
-        setLoading(false);
-        setOpen(false);
-      }
+  };
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push("/");
+      toast.success("Store Deleted!!!");
+    } catch (error) {
+      toast.error(
+        "Make sure you have removed all products and categories first."
+      );
+    } finally {
+      setLoading(false);
+      setOpen(false);
     }
+  };
   return (
     <>
-    <AlertModal
-    isOpen={open}
-    onClose={()=>setOpen(false)}
-    onConfirm={onDelete}
-    loading={loading}
-    />
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading title="Setting" description="manage store preference" />
-        <Button disabled={loading} variant={"destructive"} size={"icon"} onClick={() => setOpen(true)}>
+        <Button
+          disabled={loading}
+          variant={"destructive"}
+          size={"icon"}
+          onClick={() => setOpen(true)}
+        >
           <Trash className="h-4 w-4" />
         </Button>
       </div>
-      <Separator/>
+      <Separator />
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} 
-        className="space-y-8 w-full"
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
         >
-            <div className="grid grid-cols-3 gap-8">
-                <FormField 
-                control={form.control}
-                name="name"
-                render={({field})=>(
-                    <FormItem>
-                        <FormLabel>Name</FormLabel>
-                        <FormControl>
-                            <Input disabled={loading} placeholder="Store Name" {...field} />
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}
-                />
-            </div>
-            <Button
-            disabled={loading} className="al-auto" type="submit"
-            >Save Changes</Button>
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store Name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="al-auto" type="submit">
+            Save Changes
+          </Button>
         </form>
       </Form>
-      <Separator/>
-      <ApiAlert title="test" description="test desc" variant="public"/>
+      <Separator />
+      <ApiAlert
+        title="NEXT_PUBLIC_API_URL"
+        description={`${origin}/api/${params.storeId}`}
+        variant="public"
+      />
     </>
   );
 };
