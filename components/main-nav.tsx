ab219867a0d3fa78b66174db227d2e59476f32d9
Nihar Hegde <niharhegde163@gmail.com>
2023-09-12 17:29:01 +0530

    adding a route to overview

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -9,7 +9,12 @@ export const MainNav = ({className,...props}:HTMLAttributes<HTMLElement>) => {
   const params = useParams();
 
     const routes = [
-        {
+      {
+        href:`/${params.storeId}`,
+        label:"OverView",
+        active:pathname === `/${params.storeId}`
+        },  
+      {
         href:`/${params.storeId}/settings`,
         label:"Settings",
         active:pathname === `/${params.storeId}/settings`
