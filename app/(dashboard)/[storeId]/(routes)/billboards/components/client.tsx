b9527abba7652c0df492280b7dd6b9b0ae7f9af1
Nihar Hegde <niharhegde163@gmail.com>
2023-09-15 21:59:23 +0530

    showing data table with all billboards

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,25 +1,37 @@
-'use client'
+"use client";
 
-import { Button } from "@/components/ui/button"
-import Heading from "@/components/ui/heading"
-import { Separator } from "@/components/ui/separator"
-import { Plus } from "lucide-react"
-import { useParams, useRouter } from "next/navigation"
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { FC } from "react";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-tabel";
 
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
 
-export const BillboardClient = () => {
-    const router = useRouter();
-    const params = useParams();
+export const BillboardClient: FC<BillboardClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
   return (
     <>
-    <div className="flex items-center justify-between">
-        <Heading title="Billboards (0)" description="Manage billboard for your store"/>
-        <Button onClick={()=> router.push(`/${params.storeId}/billboards/new`)}>
-            <Plus className="mr-2 h-4 w-4"/>
-            Add new
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage billboard for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add new
         </Button>
-    </div>
-    <Separator/>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data}/>
     </>
-  )
-}
+  );
+};
