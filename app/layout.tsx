22dc5798f6bd4bfc05d6ba818c94358456e4d241
Nihar Hegde <niharhegde163@gmail.com>
2023-10-12 21:56:55 +0530

    adding dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
 import { ToasterProvider } from "@/providers/toast-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,14 +18,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToasterProvider/>
-          <ModalProvider/>
-          {children}</body>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <ToasterProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,32 +1,34 @@
-import { UserButton, auth } from '@clerk/nextjs'
-import React from 'react'
-import {MainNav} from '@/components/main-nav'
-import StoreSwitcher from '@/components/store-switcher'
-import { redirect } from 'next/navigation'
-import prismadb from '@/lib/prismadb'
+import { UserButton, auth } from "@clerk/nextjs";
+import React from "react";
+import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
+import { ModeToggle } from "./theme-toggle";
 
 const Navbar = async () => {
-    const {userId} = auth();
-    if(!userId){
-        redirect('/sign-in');
-    }
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
 
-    const stores = await prismadb.store.findMany({
-        where:{
-            userId
-        }
-    })
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
-    <div className='border-b'>
-        <div className="flex h-16 items-center px-4">
-            <StoreSwitcher items={stores}/>
-            <MainNav className='mx-6'/>
-            <div className='ml-auto flex items-center space-x-4'>
-                <UserButton afterSignOutUrl='/'/>
-            </div>
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <ModeToggle />
+          <UserButton afterSignOutUrl="/" />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
